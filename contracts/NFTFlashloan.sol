72d4491f6983e23d0882eaaae870fa97cdc39852
Adam H <surfcoderepeat@protonmail.com>
2021-07-20 19:52:33 -0600

    first commit

---
+++ contracts/NFTFlashloan.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "../interfaces/INFT20Pair.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "../interfaces/IFlashLoanReceiver.sol";
+
+contract NFTFlashloan is IFlashLoanReceiver, IERC721Receiver {
+    IERC721 public constant HASHMASK =
+        IERC721(0xC2C747E0F7004F9E8817Db2ca4997657a7746928);
+    INFT20Pair public constant MASK20 =
+        INFT20Pair(0xc2BdE1A2fA26890c8E6AcB10C91CC6D9c11F4a73);
+
+    constructor() public {
+        HASHMASK.setApprovalForAll(address(MASK20), true); //must approve nft20 pair contract for spending
+    }
+
+    function executeOperation(
+        uint256[] calldata _ids,
+        uint256[] calldata _amounts,
+        address initiator,
+        bytes calldata params
+    ) external override returns (bool) {
+        // Do stuff with the tokens nft ids
+        for (uint256 i = 0; i < _ids.length; i++) {
+            //   do something with nfts but you must return them to the same amount of nfts to contract after
+        }
+        return true;
+    }
+
+    function onERC721Received(
+        address operator,
+        address,
+        uint256,
+        bytes memory data
+    ) public virtual override returns (bytes4) {
+        return this.onERC721Received.selector;
+    }
+}
---
+++ test/flashLoan.js
@@ -0,0 +1,72 @@
+// For mainnet block number 12069925 with this command:
+// npx hardhat node --fork https://mainnet.infura.io/v3/INFURA_KEY  --fork-block-number 12069925
+
+const { assert, expect } = require("chai");
+
+const NFT20_PAIR = "0xc2bde1a2fa26890c8e6acb10c91cc6d9c11f4a73";
+const MEME_PAIR = "0x60acd58d00b2bcc9a8924fdaa54a2f7c0793b3b2";
+let account = "0x4B5922ABf25858d012d12bb1184e5d3d0B6D6BE4";
+
+// TODO Tokens you want to loan from the NFT20 pool out
+const TOKENS = [
+	930,
+	14344,
+	2248,
+	14483,
+	13728,
+	13844,
+	9433,
+	10046,
+	10229,
+	14369,
+	12790,
+	4514,
+	4976,
+	10185,
+	13954,
+	15634,
+	14985,
+	4143,
+	14381,
+	12412,
+];
+
+const MEME_TOKENS = [1];
+
+const hre = require("hardhat");
+
+describe("Contracts", () => {
+	before(async () => {
+		await hre.network.provider.request({
+			method: "hardhat_impersonateAccount",
+			params: [account],
+		});
+		signer = await ethers.provider.getSigner(account);
+
+		hashMasks = await ethers.getContractAt(
+			"ERC721",
+			"0xC2C747E0F7004F9E8817Db2ca4997657a7746928"
+		);
+
+		Factory = await ethers.getContractAt(
+			"NFT20FactoryV4",
+			"0x0f4676178b5c53Ae0a655f1B19A96387E4b8B5f2"
+		);
+
+		HashMaskPair = await ethers.getContractAt("INFT20Pair", NFT20_PAIR); //nft20 pool for hashmask
+
+		Flashloan = await ethers.getContractFactory("NFTFlashloan");
+		flashloan = await Flashloan.deploy();
+	});
+
+	it("should execute flash loan", async function () {
+		console.log("Prepare flashloan", await Factory.logic());
+
+		let fl_tx = await HashMaskPair.connect(signer).flashLoan(
+			TOKENS, //array of ids to loan
+			[1], //array of amoutns to laon, for erc721 you can just send like this
+			flashloan.address, //contract of flash loan address
+			"0x"
+		);
+	});
+});
